58bfa2bf0d4074956aae05e44985ba195185377d
Andres Villagra <89531860+KaiserOfNone@users.noreply.github.com>
2025-09-15 19:54:20 -0300

    Added the bouncer back as a Tiler

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use qrcode_generator::QrCodeEcc;
 
 /*
 * TODO list:
- * - Add a basic tiler for regular ass QRs
  * - CLI calling for non hardcoded shit
  * - Add the alignment calculator function
  * - Add a "cleanup" step to add the alignment markers
@@ -80,11 +79,54 @@ impl Tiler for BaseTiler {
     }
 }
 
+struct BouncyTiler {
+    block_size: u32,
+    image: RgbaImage,
+    sheet: RgbaImage,
+    bounce: bool,
+}
+
+impl BouncyTiler {
+    fn new(width: u32, sheet: RgbaImage) -> BouncyTiler {
+        let block_size = 32; // Constant from the sprite I'm using
+        BouncyTiler {
+            block_size,
+            image: RgbaImage::new(width * block_size, width * block_size),
+            sheet,
+            bounce: false,
+        }
+    }
+}
+
+impl Tiler for BouncyTiler {
+    fn tile(&mut self, x: u32, y: u32, value: bool) {
+        let mut x0 = if self.bounce { 32 } else { 0 };
+        x0 = if value { x0 + 64 } else { x0 };
+        let dst_x = x * self.block_size;
+        let dst_y = y * self.block_size;
+        copy_paste(
+            &self.sheet,
+            &mut self.image,
+            x0,
+            0,
+            self.block_size,
+            self.block_size,
+            dst_x,
+            dst_y,
+        );
+        self.bounce = !self.bounce;
+    }
+
+    fn finalize(&self) -> RgbaImage {
+        self.image.clone()
+    }
+}
+
 fn main() {
     let sheet = image::open("sheet.png").unwrap().into_rgba8();
 
     let result: Vec<Vec<bool>> = qrcode_generator::to_matrix("Lucas", QrCodeEcc::Quartile).unwrap();
-    let mut tiler = BaseTiler::new(32, result.len() as u32);
+    let mut tiler = BouncyTiler::new(result.len() as u32, sheet);
 
     for (y, row) in result.iter().enumerate() {
         for (x, val) in row.iter().enumerate() {
@@ -92,19 +134,4 @@ fn main() {
         }
     }
     tiler.finalize().save("test.png").unwrap();
-    /*
-    let mut result_image = RgbaImage::new(resolution, resolution);
-    let mut bouncer = false;
-    for (y, row) in result.iter().enumerate() {
-        for (x, val) in row.iter().enumerate() {
-            let mut x0 = if bouncer { 32 } else { 0 };
-            x0 = if *val { x0 + 64 } else { x0 };
-            let dst_x = (x * 32) as u32;
-            let dst_y = (y * 32) as u32;
-            copy_paste(&sheet, &mut result_image, x0, 0, 32, 32, dst_x, dst_y);
-            bouncer = !bouncer;
-        }
-    }
-    result_image.save("test.png").unwrap();
-    */
 }
